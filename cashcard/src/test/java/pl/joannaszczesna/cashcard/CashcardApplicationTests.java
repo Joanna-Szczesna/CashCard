b8c0a5b9a4419a7c0fd33b5431cb22f713b80bd2
Joanna <j.k.szczesna@gmail.com>
2024-10-23 12:42:10 +0200

    Get single card full resource TDD_RED
    check response status and body

--- cashcard/src/test/java/pl/joannaszczesna/cashcard/CashcardApplicationTests.java
+++ cashcard/src/test/java/pl/joannaszczesna/cashcard/CashcardApplicationTests.java
@@ -1,5 +1,7 @@
 package pl.joannaszczesna.cashcard;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +23,14 @@ class CashcardApplicationTests {
         @Test
         void shouldReturnACashCardWhenDataIsSaved() {
             ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
-
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+            DocumentContext documentContext = JsonPath.parse(response.getBody());
+            Number id = documentContext.read("$.id");
+            assertThat(id).isEqualTo(99);
+
+            Double amount = documentContext.read("$.amount");
+            assertThat(amount).isEqualTo(123.45);
         }
     }
 
