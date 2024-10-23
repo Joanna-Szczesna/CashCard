11c47b219311ec689977cc2df46024129bf00155
Joanna <j.k.szczesna@gmail.com>
2024-10-23 13:06:15 +0200

    Add Repository and database H2

--- cashcard/src/main/java/pl/joannaszczesna/cashcard/CashCard.java
+++ cashcard/src/main/java/pl/joannaszczesna/cashcard/CashCard.java
@@ -1,4 +1,5 @@
 package pl.joannaszczesna.cashcard;
+import org.springframework.data.annotation.Id;
 
-record CashCard(Long id, Double amount) {
+record CashCard(@Id Long id, Double amount) {
 }
---
+++ cashcard/src/main/java/pl/joannaszczesna/cashcard/CashCardRepository.java
@@ -0,0 +1,6 @@
+package pl.joannaszczesna.cashcard;
+
+import org.springframework.data.repository.CrudRepository;
+
+interface CashCardRepository extends CrudRepository<CashCard, Long> {
+}
