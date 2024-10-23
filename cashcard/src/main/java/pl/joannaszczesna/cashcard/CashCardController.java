4cdc6e654d557b86e2b5f90fbb0e90aff6251cb8
Joanna <j.k.szczesna@gmail.com>
2024-10-23 12:49:29 +0200

    Get @PathVariable TDD_GREEN

--- cashcard/src/main/java/pl/joannaszczesna/cashcard/CashCardController.java
+++ cashcard/src/main/java/pl/joannaszczesna/cashcard/CashCardController.java
@@ -2,6 +2,7 @@ package pl.joannaszczesna.cashcard;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CashCardController {
 
     @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById() {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard );
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        if(requestedId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
