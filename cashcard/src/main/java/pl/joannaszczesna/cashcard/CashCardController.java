3bdd3845cbef50dc92d60ccc2111c8d48a46f159
Joanna <j.k.szczesna@gmail.com>
2024-10-23 14:20:02 +0200

    POST endpoint TDD_GREEN

--- cashcard/src/main/java/pl/joannaszczesna/cashcard/CashCardController.java
+++ cashcard/src/main/java/pl/joannaszczesna/cashcard/CashCardController.java
@@ -1,11 +1,10 @@
 package pl.joannaszczesna.cashcard;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 
@@ -25,4 +24,24 @@ public class CashCardController {
         }
         return ResponseEntity.notFound().build();
     }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
+        /*
+        return ResponseEntity.created(locationOfNewCashCard).build();
+             IS EQUAL:
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .header(HttpHeaders.LOCATION, uriOfCashCard.toASCIIString())
+            .build();
+        */
+
+    }
+
 }
