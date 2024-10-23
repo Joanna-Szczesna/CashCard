348fc171d76e20f87562c2f0b92296402fdade40
Joanna <j.k.szczesna@gmail.com>
2024-10-23 12:31:28 +0200

    Get single card TDD_RED

--- cashcard/src/test/java/pl/joannaszczesna/cashcard/CashcardApplicationTests.java
+++ cashcard/src/test/java/pl/joannaszczesna/cashcard/CashcardApplicationTests.java
@@ -1,13 +1,30 @@
 package pl.joannaszczesna.cashcard;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashcardApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Nested
+    class GetRead{
+        @Test
+        void shouldReturnACashCardWhenDataIsSaved() {
+            ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        }
     }
+
 
 }
