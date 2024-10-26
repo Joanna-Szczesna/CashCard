4ee46c34a061f2273cbc03bc1214bdd0b407ec18
Joanna <j.k.szczesna@gmail.com>
2024-10-26 16:20:17 +0200

    add default value for sorting Get list endpoint TDD_GREEN

--- cashcard/src/main/java/pl/joannaszczesna/cashcard/CashCardController.java
+++ cashcard/src/main/java/pl/joannaszczesna/cashcard/CashCardController.java
@@ -1,8 +1,9 @@
 package pl.joannaszczesna.cashcard;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -20,6 +21,7 @@ public class CashCardController {
     private CashCardController(CashCardRepository cashCardRepository) {
         this.cashCardRepository = cashCardRepository;
     }
+
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
         Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
@@ -54,7 +56,7 @@ public class CashCardController {
                 PageRequest.of(
                         pageable.getPageNumber(),
                         pageable.getPageSize(),
-                        pageable.getSort()
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
                 ));
         return ResponseEntity.ok(page.getContent());
     }
